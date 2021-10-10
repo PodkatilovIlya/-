8c8369a6d63528e33209045837720f00a080b721
Podkatilov Ilya <ilya.podkatilov@aisa.ru>
2021-10-10 16:43:41 +0300

    test

---
+++ precache-manifest.f9d6f37927d584f5b66b794e696e90b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4367e973608d0891e843",
+    "url": "/css/chunk-vendors.7d0ebb8b.css"
+  },
+  {
+    "revision": "06b3082016d3ba6cc44c3dd1ce7a418e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de5c226f3ce28bb28ea5",
+    "url": "/js/app.e2fe11c0.js"
+  },
+  {
+    "revision": "4367e973608d0891e843",
+    "url": "/js/chunk-vendors.e697fa49.js"
+  },
+  {
+    "revision": "5f3c490bb0b3f8a3d17ac39c5a39420e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
